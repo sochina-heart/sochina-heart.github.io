b6ea187adfde4157944b38d5d35dfff2d60689c5
heart-sochina <3014862187@qq.com>
2024-02-24 12:25:36 +0800

    Site updated: 2024-02-24 12:25:35

--- js/comment.js
+++ js/comment.js
@@ -240,8 +240,6 @@ var _renderHTML = function _renderHTML(params) {
       <span class="discussion-item-icon">
         <svg aria-hidden="true" class="octicon octicon-tag" height="16" version="1.1" viewBox="0 0 16 16" width="14"><path fill-rule="evenodd" d="M15 1H6c-.55 0-1 .45-1 1v2H1c-.55 0-1 .45-1 1v6c0 .55.45 1 1 1h1v3l3-3h4c.55 0 1-.45 1-1V9h1l3 3V9h1c.55 0 1-.45 1-1V2c0-.55-.45-1-1-1zM9 11H4.5L3 12.5V11H1V5h4v3c0 .55.45 1 1 1h3v2zm6-3h-2v1.5L11.5 8H6V2h9v6z"></path></svg>
       </span>
-      The above comments are provided by 
-      <a href="http://github.com/wzpan/comment.js" class="discussion-item-entity" target="_blank">comment.js</a> with the help of ${site}.
     </h3>
 </div>
     `;
